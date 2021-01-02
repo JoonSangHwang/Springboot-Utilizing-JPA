492352a7f52e2b504ebb0079e4f2669e43ada4ae
JoonsangHwang <gufrus@naver.com>
2021-01-02 23:33:14 +0900

    [2021.01.02] 도메인 모델 초기화

---
+++ example/src/main/java/com/joonsang/example/domain/DeliveryStatus.java
@@ -0,0 +1,5 @@
+package com.joonsang.example.domain;
+
+public enum DeliveryStatus {
+    READY, COMP
+}
---
+++ example/src/main/java/com/joonsang/example/domain/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.joonsang.example.domain;
+
+public enum OrderStatus {
+    ORDER, CANCEL
+}
